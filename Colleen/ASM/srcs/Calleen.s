eb231b97cfa689dd4b1b8c618f17a98ebd9ffbd3
Pioupia <pioupiadeux@gmail.com>
2026-04-24 09:49:26 +0200

    Fix(Calleen): ASM issue with rodata section

--- Colleen/ASM/srcs/Calleen.s
+++ Colleen/ASM/srcs/Calleen.s
@@ -29,4 +29,4 @@ main:
 
 
 section .rodata
-	text: db "section .text%1$calign 16%1$c%1$cSYS_EXIT%2$cequ 60%1$c%1$c; This is one comment%1$cglobal main%1$c%1$cextern printf%1$cextern exit%1$c%1$cget_ptr:%1$c%2$cmov rax, text%1$c%2$cret%1$c%1$cmain:%1$c%2$ccall get_ptr%1$c%2$cmov rdi, rax%1$c%2$cmov rsi, 10%1$c%2$cmov rdx, 9%1$c%2$cmov rcx, 34%1$c%2$cmov r8, rax%1$c%2$csub rsp, 8%1$c%2$ccall printf%1$c%2$c; call exit(0)%1$c%2$cxor rdi, rdi%1$c%2$ccall exit%1$c%2$cret%1$c%1$c%1$csection .rdata%1$c%2$ctext: db %3$c%4$s%3$c, 0%1$c", 0
+	text: db "section .text%1$calign 16%1$c%1$cSYS_EXIT%2$cequ 60%1$c%1$c; This is one comment%1$cglobal main%1$c%1$cextern printf%1$cextern exit%1$c%1$cget_ptr:%1$c%2$cmov rax, text%1$c%2$cret%1$c%1$cmain:%1$c%2$ccall get_ptr%1$c%2$cmov rdi, rax%1$c%2$cmov rsi, 10%1$c%2$cmov rdx, 9%1$c%2$cmov rcx, 34%1$c%2$cmov r8, rax%1$c%2$csub rsp, 8%1$c%2$ccall printf%1$c%2$c; call exit(0)%1$c%2$cxor rdi, rdi%1$c%2$ccall exit%1$c%2$cret%1$c%1$c%1$csection .rodata%1$c%2$ctext: db %3$c%4$s%3$c, 0%1$c", 0
